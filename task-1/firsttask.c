b1682c3bbbbd4a085341efabcbab33e63cf4363f
Mohammad Faraz <its_farzi@outlook.com>
2026-03-26 10:40:01 +0530

    gy

---
+++ task-1/firsttask.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int x = 10;
+    int age = 26;
+    float price = 99.5;
+    char grade = 'a';
+    printf("x= %d", x);
+    printf("\nage= %d", age);
+    printf("\nprice= %f", price);
+    printf("\ngrade= %c", grade);
+
+    
+    return 0;
+}
